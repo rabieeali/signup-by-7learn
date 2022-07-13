2f83dfc6e5a156ce71e203b4fccc4db387c85cb9
ali.rabiee <a.rabiee@fidilio.com>
2022-07-13 20:25:40 +0430

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import { AuthProvider } from './Context/auth-context';
+
+ReactDOM.render(
+  <AuthProvider>
+    <App />
+  </AuthProvider>
+  ,
+  document.getElementById('root')
+);
